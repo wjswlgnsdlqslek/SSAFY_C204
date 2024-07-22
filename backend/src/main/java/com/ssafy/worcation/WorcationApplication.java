d96aa0360692d894ab382943447cd1b127cff4d6
이병수 <evil55@naver.com>
2024-07-22 14:24:52 +0900

    init

---
+++ backend/src/main/java/com/ssafy/worcation/WorcationApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.worcation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorcationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorcationApplication.class, args);
+	}
+
+}
