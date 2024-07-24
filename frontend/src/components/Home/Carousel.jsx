85e72ee4f7047adc319128eeed311928931b1974
전지훈 <ggh2111@naver.com>
2024-07-25 02:02:59 +0900

    design(메인): 메인 페이지 css 수정 서비스 소개, 기대 효과 못 함

--- frontend/src/components/Home/Carousel.jsx
+++ frontend/src/components/Home/Carousel.jsx
@@ -1,3 +1,175 @@
+// import React from "react";
+// import Carousel from "react-multi-carousel";
+// import "react-multi-carousel/lib/styles.css";
+
+// const responsive = {
+//   desktop: {
+//     breakpoint: { max: 3000, min: 1024 },
+//     items: 3,
+//     partialVisibilityGutter: 40,
+//   },
+//   tablet: {
+//     breakpoint: { max: 1024, min: 464 },
+//     items: 1,
+//     partialVisibilityGutter: 30,
+//   },
+//   mobile: {
+//     breakpoint: { max: 464, min: 0 },
+//     items: 1,
+//     partialVisibilityGutter: 30,
+//   },
+// };
+
+// const CarouselComponent = () => {
+//   return (
+//     <Carousel
+//       additionalTransfrom={0}
+//       arrows
+//       autoPlay
+//       autoPlaySpeed={4500}
+//       centerMode={false}
+//       className=""
+//       containerClass="container-with-dots"
+//       dotListClass=""
+//       draggable
+//       focusOnSelect={false}
+//       infinite
+//       itemClass=""
+//       keyBoardControl
+//       minimumTouchDrag={80}
+//       pauseOnHover
+//       renderArrowsWhenDisabled={false}
+//       renderButtonGroupOutside={false}
+//       renderDotsOutside={false}
+//       responsive={responsive}
+//       rewind={false}
+//       rewindWithAnimation={false}
+//       rtl={false}
+//       shouldResetAutoplay
+//       showDots={false}
+//       sliderClass=""
+//       slidesToSlide={1}
+//       swipeable
+//     >
+//       {/* 기본 카드 형태
+// 			<div className="card w-96 bg-base-100 shadow-xl mx-auto">
+//         <figure>
+//           <img src="/assets/팜플렛/강원 팜플렛.png" alt="Item 1" />
+//         </figure>
+//         <div className="card-body items-center p-4">
+//           <h2 className="card-title">강원특별자치도</h2>
+//           <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn text-mainTxt hover:bg-mainBlue">
+//             이동하기
+//           </button>
+//         </div>
+//       </div> */}
+
+//       <div className="card glass w-96 mx-auto">
+//         <figure className="relative">
+//           <img src="/assets/팜플렛/강원 팜플렛.png" alt="Item 1" />
+//           <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
+//             이동하기
+//           </button>
+//         </figure>
+//         <div className="card-body items-center p-4">
+//           <h2 className="card-title">강원특별자치도</h2>
+//         </div>
+//       </div>
+
+//       <div className="card glass w-96 mx-auto">
+//         <figure className="relative">
+//           <img src="/assets/팜플렛/더 휴일 팜플렛.png" alt="Item 2" />
+//           <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
+//             이동하기
+//           </button>
+//         </figure>
+//         <div className="card-body items-center p-4">
+//           <h2 className="card-title">더 휴일</h2>
+//         </div>
+//       </div>
+//       <div className="card glass w-96 mx-auto">
+//         <figure className="relative">
+//           <img src="/assets/팜플렛/데스커 팜플렛.png" alt="Item 3" />
+//           <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
+//             이동하기
+//           </button>
+//         </figure>
+//         <div className="card-body items-center p-4">
+//           <h2 className="card-title">데스커</h2>
+//         </div>
+//       </div>
+//       <div className="card glass w-96 mx-auto">
+//         <figure className="relative">
+//           <img src="/assets/팜플렛/부산 팜플렛.png" alt="Item 4" />
+//           <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
+//             이동하기
+//           </button>
+//         </figure>
+//         <div className="card-body items-center p-4">
+//           <h2 className="card-title">부산</h2>
+//         </div>
+//       </div>
+//       <div className="card glass w-96 mx-auto">
+//         <figure className="relative">
+//           <img src="/assets/팜플렛/서울 팜플렛.png" alt="Item 5" />
+//           <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
+//             이동하기
+//           </button>
+//         </figure>
+//         <div className="card-body items-center p-4">
+//           <h2 className="card-title">서울</h2>
+//         </div>
+//       </div>
+//       <div className="card glass w-96 mx-auto">
+//         <figure className="relative">
+//           <img src="/assets/팜플렛/어촌체험 휴양마을 팜플렛.png" alt="Item 6" />
+//           <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
+//             이동하기
+//           </button>
+//         </figure>
+//         <div className="card-body items-center p-4">
+//           <h2 className="card-title">어촌체험 휴양마을</h2>
+//         </div>
+//       </div>
+//       <div className="card glass w-96 mx-auto">
+//         <figure className="relative">
+//           <img src="/assets/팜플렛/전북 팜플렛.png" alt="Item 7" />
+//           <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
+//             이동하기
+//           </button>
+//         </figure>
+//         <div className="card-body items-center p-4">
+//           <h2 className="card-title">전북특별자치도</h2>
+//         </div>
+//       </div>
+//       <div className="card glass w-96 mx-auto">
+//         <figure className="relative">
+//           <img src="/assets/팜플렛/제주 팜플렛.png" alt="Item 8" />
+//           <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
+//             이동하기
+//           </button>
+//         </figure>
+//         <div className="card-body items-center p-4">
+//           <h2 className="card-title">제주도</h2>
+//         </div>
+//       </div>
+//       <div className="card glass w-96 mx-auto">
+//         <figure className="relative">
+//           <img src="/assets/팜플렛/충남 팜플렛.png" alt="Item 9" />
+//           <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
+//             이동하기
+//           </button>
+//         </figure>
+//         <div className="card-body items-center p-4">
+//           <h2 className="card-title">충청남도</h2>
+//         </div>
+//       </div>
+//     </Carousel>
+//   );
+// };
+
+// export default CarouselComponent;
+
 import React from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
@@ -20,7 +192,59 @@ const responsive = {
   },
 };
 
+const carouselItems = [
+  {
+    title: "강원특별자치도",
+    image: "/assets/팜플렛/강원 팜플렛.png",
+    url: "https://worcation.co.kr/gw",
+  },
+  {
+    title: "더 휴일",
+    image: "/assets/팜플렛/더 휴일 팜플렛.png",
+    url: "https://thehyuil.co.kr/",
+  },
+  {
+    title: "데스커",
+    image: "/assets/팜플렛/데스커 팜플렛.png",
+    url: "https://www.deskerworkation.com/",
+  },
+  {
+    title: "부산",
+    image: "/assets/팜플렛/부산 팜플렛.png",
+    url: "https://www.busaness.com/",
+  },
+  {
+    title: "서울",
+    image: "/assets/팜플렛/서울 팜플렛.png",
+    url: "https://worcation.sba.kr/",
+  },
+  {
+    title: "어촌체험 휴양마을",
+    image: "/assets/팜플렛/어촌체험 휴양마을 팜플렛.png",
+    url: "https://seantour.kr/newseantour/main/main.do",
+  },
+  {
+    title: "전북특별자치도",
+    image: "/assets/팜플렛/전북 팜플렛.png",
+    url: "https://jb-worcation.com/",
+  },
+  {
+    title: "제주도",
+    image: "/assets/팜플렛/제주 팜플렛.png",
+    url: "https://jejuworkation.or.kr/",
+  },
+  {
+    title: "충청남도",
+    image: "/assets/팜플렛/충남 팜플렛.png",
+    url: "https://cnctf.or.kr/site/workation/main.php",
+  },
+];
+
 const CarouselComponent = () => {
+  const openInNewTab = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
+
   return (
     <Carousel
       additionalTransfrom={0}
@@ -51,119 +275,22 @@ const CarouselComponent = () => {
       slidesToSlide={1}
       swipeable
     >
-      {/* 기본 카드 형태 
-			<div className="card w-96 bg-base-100 shadow-xl mx-auto">
-        <figure>
-          <img src="/assets/팜플렛/강원 팜플렛.png" alt="Item 1" />
-        </figure>
-        <div className="card-body items-center p-4">
-          <h2 className="card-title">강원특별자치도</h2>
-          <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn text-mainTxt hover:bg-mainBlue">
-            이동하기
-          </button>
+      {carouselItems.map((item, index) => (
+        <div key={index} className="card bg-transparent w-96 mx-auto">
+          <figure className="relative">
+            <img src={item.image} alt={item.title} />
+            <button
+              className="btn bg-transparent border border-white absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white"
+              onClick={() => openInNewTab(item.url)}
+            >
+              이동하기
+            </button>
+          </figure>
+          <div className="card-body items-center p-4 bg-transparent">
+            <h2 className="card-title text-white font-bold">{item.title}</h2>
+          </div>
         </div>
-      </div> */}
-
-      <div className="card glass w-96 mx-auto">
-        <figure className="relative">
-          <img src="/assets/팜플렛/강원 팜플렛.png" alt="Item 1" />
-          <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
-            이동하기
-          </button>
-        </figure>
-        <div className="card-body items-center p-4">
-          <h2 className="card-title">강원특별자치도</h2>
-        </div>
-      </div>
-
-      <div className="card glass w-96 mx-auto">
-        <figure className="relative">
-          <img src="/assets/팜플렛/더 휴일 팜플렛.png" alt="Item 2" />
-          <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
-            이동하기
-          </button>
-        </figure>
-        <div className="card-body items-center p-4">
-          <h2 className="card-title">더 휴일</h2>
-        </div>
-      </div>
-      <div className="card glass w-96 mx-auto">
-        <figure className="relative">
-          <img src="/assets/팜플렛/데스커 팜플렛.png" alt="Item 3" />
-          <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
-            이동하기
-          </button>
-        </figure>
-        <div className="card-body items-center p-4">
-          <h2 className="card-title">데스커</h2>
-        </div>
-      </div>
-      <div className="card glass w-96 mx-auto">
-        <figure className="relative">
-          <img src="/assets/팜플렛/부산 팜플렛.png" alt="Item 4" />
-          <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
-            이동하기
-          </button>
-        </figure>
-        <div className="card-body items-center p-4">
-          <h2 className="card-title">부산</h2>
-        </div>
-      </div>
-      <div className="card glass w-96 mx-auto">
-        <figure className="relative">
-          <img src="/assets/팜플렛/서울 팜플렛.png" alt="Item 5" />
-          <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
-            이동하기
-          </button>
-        </figure>
-        <div className="card-body items-center p-4">
-          <h2 className="card-title">서울</h2>
-        </div>
-      </div>
-      <div className="card glass w-96 mx-auto">
-        <figure className="relative">
-          <img src="/assets/팜플렛/어촌체험 휴양마을 팜플렛.png" alt="Item 6" />
-          <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
-            이동하기
-          </button>
-        </figure>
-        <div className="card-body items-center p-4">
-          <h2 className="card-title">어촌체험 휴양마을</h2>
-        </div>
-      </div>
-      <div className="card glass w-96 mx-auto">
-        <figure className="relative">
-          <img src="/assets/팜플렛/전북 팜플렛.png" alt="Item 7" />
-          <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
-            이동하기
-          </button>
-        </figure>
-        <div className="card-body items-center p-4">
-          <h2 className="card-title">전북특별자치도</h2>
-        </div>
-      </div>
-      <div className="card glass w-96 mx-auto">
-        <figure className="relative">
-          <img src="/assets/팜플렛/제주 팜플렛.png" alt="Item 8" />
-          <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
-            이동하기
-          </button>
-        </figure>
-        <div className="card-body items-center p-4">
-          <h2 className="card-title">제주도</h2>
-        </div>
-      </div>
-      <div className="card glass w-96 mx-auto">
-        <figure className="relative">
-          <img src="/assets/팜플렛/충남 팜플렛.png" alt="Item 9" />
-          <button className="btn glass absolute bottom-4 left-1/2 transform -translate-x-1/2">
-            이동하기
-          </button>
-        </figure>
-        <div className="card-body items-center p-4">
-          <h2 className="card-title">충청남도</h2>
-        </div>
-      </div>
+      ))}
     </Carousel>
   );
 };
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import React from "react";
 import Carousel from "../components/Home/Carousel";
-import IntroduceWokcation from "../components/Home/IntroduceWorkcation";
+import IntroduceWorkcation from "../components/Home/IntroduceWorkcation";
 
 function HomePage() {
   return (
-    <div className="px-24">
-      {/* Hero Section 메인 이미지와 소개 문구 */}
-      <section id="서비스 소개" className="py-16 bg-gray-100 text-center">
+    <div className="max-w-screen-xl mx-auto">
+      {/* 메인 Section */}
+      <section id="서비스 소개" className="py-16 bg-gray-100 text-center px-6">
         <h1 className="text-4xl font-bold">
           유저가 워케이션을 효과적으로 계획하고 즐길 수 있도록 다양한 정보를
           제공합니다.
@@ -16,56 +16,35 @@ function HomePage() {
           즐기세요!
         </p>
         <img
-          src="/hero-image.png"
+          src="/api/placeholder/400/300"
           alt="Hero"
           className="w-4/5 max-w-lg mx-auto my-6"
         />
       </section>
 
-      {/* 워케이션 소게 Section */}
-      {/* <section id="워케이션" className="py-16 bg-white text-center">
-        <div className="flex">
-          <div>
-            <img
-              src="/assets/소개/워케이션이란.png"
-              alt="워케이션"
-              className="w-4/5 max-w-lg mx-auto my-6"
-            />
-            <img
-              src="/assets/소개/인식.png"
-              alt="인식"
-              className="w-4/5 max-w-lg mx-auto my-6"
-            />
-            <small>
-              <small>
-                <small>츨처: 한국관광공사</small>
-              </small>
-            </small>
-          </div>
-          <div>
-            <h3>워케이션이란?</h3>
-            <p>
-              Work(일), Vacation(휴가)의 합성어로, 집과 사무실에서 벗어나
-              휴가지에서 업무와 휴식을 동시에 경험하는 새로운 근무제도를
-              뜻합니다. WAVA에서 일(Work)상에서 쉼표가 되는 순간을 기대해주세요!
-            </p>
-          </div>
-        </div>
-      </section> */}
-      <section id="워케이션" className="py-12 bg-white text-center">
-        <IntroduceWokcation />
+      {/* 워케이션 소개 Section */}
+      <section id="워케이션" className="py-12 bg-white text-center px-6">
+        <IntroduceWorkcation />
       </section>
 
       {/* 진행중인 워케이션 */}
-      <section id="진행중인 워케이션" className="py-12 bg-gray-100 text-center">
+      <section
+        id="진행중인 워케이션"
+        className="py-12 text-center px-6 bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: "url('/assets/팜플렛/부산 팜플렛.png')" }}
+      >
         <h2 className="text-3xl font-semibold mb-5">진행중인 워케이션</h2>
         <Carousel />
       </section>
 
       {/* 기대효과 Section */}
-      <section id="기대효과" className="py-16 bg-white text-center">
+      <section id="기대효과" className="py-16 bg-white text-center px-6">
         <h2 className="text-3xl font-semibold">기대효과</h2>
-        <img src="/vase.png" alt="Vase" className="w-72 mx-auto my-6" />
+        <img
+          src="/api/placeholder/288/288"
+          alt="Vase"
+          className="w-72 mx-auto my-6"
+        />
         <p className="text-lg">
           Elegant and unique vases to enhance your decor.
         </p>
