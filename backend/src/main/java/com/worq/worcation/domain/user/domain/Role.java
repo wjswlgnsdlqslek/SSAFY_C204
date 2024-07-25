1c7e2cd68391f8faa34fb78f99a3182250ac9c96
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2024-07-25 14:56:21 +0900

    feat(유저): 유저 권한 추가

---
+++ backend/src/main/java/com/worq/worcation/domain/user/domain/Role.java
@@ -0,0 +1,5 @@
+package com.worq.worcation.domain.user.domain;
+
+public enum Role {
+    ROLE_VISITOR,ROLE_ADMIN
+}
--- backend/src/main/java/com/worq/worcation/domain/user/domain/User.java
+++ backend/src/main/java/com/worq/worcation/domain/user/domain/User.java
@@ -1,14 +1,22 @@
 package com.worq.worcation.domain.user.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
@@ -37,4 +45,19 @@ public class User {
 
     @Column(name = "user_report_count")
     private Long report;
+
+    @Enumerated(EnumType.STRING)
+    @JsonIgnore
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(this.role.toString()));
+        return authorities;
+    }
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
 }
