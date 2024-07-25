44d6c1b2ed621dbaffe0ce98b7dbb190e6e230c2
한세훈 <atdark04@gmail.com>
2024-07-25 14:58:22 +0900

    fix(대시보드페이지): 공통컴포넌트 수정
    Explorer 구조 수정

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,11 +1,10 @@
-import Explorer from "../components/Dashboard/Explorer";
 import DashboardContent from "../components/Dashboard/DashboardContent";
 import Calendar from "../components/Dashboard/Calendar";
 import useDeviceStore from "../store/deviceStore";
 import { useEffect, useRef, useState } from "react";
-import { useLocation } from "react-router-dom";
 import dayjs from "dayjs";
 import useTodoStore from "../store/todoStore";
+import Explorer from "../components/common/Explorer";
 
 function DashboardPage() {
   const [recentEvents, setRecentEvents] = useState([]);
@@ -18,17 +17,14 @@ function DashboardPage() {
   }, [events]);
 
   const handleChangeCalendarView = (viewType) => {
-    let calendarApi = calendarRef.current?.getApi();
-    let events = calendarApi.getEvents();
+    // let calendarApi = calendarRef.current?.getApi();
+    // let events = calendarApi.getEvents();
     // console.log(events);
     // calendarApi.changeView(viewType);
     console.log(recentEvents);
   };
 
   const handleDatesSet = () => {
-    let calendarApi = calendarRef.current.getApi();
-    // let events = calendarApi.getEvents();
-
     let today = dayjs();
     let start = today.subtract(6, "day").startOf("day"); // 일주일 전
     let end = today.add(1, "day").startOf("day"); // 내일의 시작 시점
@@ -42,14 +38,7 @@ function DashboardPage() {
 
   return (
     <div className="flex h-screen ">
-      {/* Explorer 컴포넌트의 너비를 비례적으로 조정 */}
-      <div
-        className={`relative ${
-          isMobile ? "w-[70px]" : "w-1/12 min-w-[80px] "
-        } max-w-[110px]`}
-      >
-        <Explorer />
-      </div>
+      <Explorer />
       {!isMobile && (
         <div className="flex-grow w-1/3 min-h-[500px] min-w-[200px]">
           <DashboardContent calendarChange={handleChangeCalendarView} />
