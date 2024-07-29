c582416efa696ceea1eb6444db89238f9bae9bbb
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2024-07-29 10:55:19 +0900

    feat: redis repository 구현

---
+++ backend/src/main/java/com/worq/worcation/common/util/RedisUtil.java
@@ -0,0 +1,27 @@
+package com.worq.worcation.common.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.TimeUnit;
+
+@Repository
+@RequiredArgsConstructor
+public class RedisUtil {
+    private final RedisTemplate<String,Object> redisTemplate;
+
+    public void setData(String key, String value, Long expiredTime) {
+        redisTemplate.opsForValue()
+                .set(key, value, expiredTime, TimeUnit.MICROSECONDS);
+    }
+
+    public String getData(String key) {
+        return (String) redisTemplate.opsForValue()
+                .get(key);
+    }
+
+    public void deleteData(String key) {
+        redisTemplate.delete(key);
+    }
+}
