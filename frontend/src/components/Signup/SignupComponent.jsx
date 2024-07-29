ef1c854046ca10e99aed1d1fbc85ab9aa52bbe24
김민주 <mj1584.mk@gmail.com>
2024-07-29 10:53:16 +0900

    design: 레이아웃 위치, 동적 속성 설정, 조건부 회원가입 버튼

--- frontend/src/components/Signup/SignupComponent.jsx
+++ frontend/src/components/Signup/SignupComponent.jsx
@@ -28,18 +28,29 @@ function SignupComponent() {
     const [isPhone, setIsPhone] = useState("");
     const [isNickName, setIsNickName] = useState("");
 
+    const [isFormVaild, setIsFormValid] = useState("");
+
+    const onChangeFormVaild = () => {
+        if (isEmail && isPassword && isPasswordConfirm && isNickName) {
+            setIsFormValid(true);
+        } else {
+            setIsFormValid(false);
+        }
+    }
+
     const onChangeEmail = (e) => {
         const currentEmail = e.target.value;
         setEmail(currentEmail);
         const emailRegEx = /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
 
         if (!emailRegEx.test(currentEmail)) {
+            setIsEmail(false);
             setEmailMessage("올바른 이메일 형식이 아닙니다.");
-            setIsEmail(false);
         } else {
+            setIsEmail(true);
             setEmailMessage("올바른 이메일 형식입니다.");
-            setIsEmail(false);
         }
+        onChangeFormVaild();
     };
 
     const onChangePassword = (e) => {
@@ -48,24 +59,26 @@ function SignupComponent() {
         const passwordRegExp = /^[A-Za-z0-9]{8,20}$/
 
         if (!passwordRegExp.test(currentPassword)) {
-            setPasswordMessage("숫자 + 영문자 조합으로 8~20자 사이로 입력해주세요.")
             setIsPassword(false);
+            setPasswordMessage("8~20자 사이의 숫자 + 영문자로 입력해주세요.")
         } else {
-            setPasswordMessage("안전한 비밀번호 입니다.")
             setIsPassword(true);
+            setPasswordMessage("안전한 비밀번호 입니다.")
         }
+        onChangeFormVaild();
     }
 
     const onChangePasswordConfirm = (e) => {
         const currentPasswordConfirm = e.target.value;
         setPasswordConfirm(currentPasswordConfirm);
         if (password !== currentPasswordConfirm) {
-            setPasswordConfirmMessage("비밀번호가 일치하지 않습니다.");
             setIsPasswordConfirm(false);
+            setPasswordConfirmMessage("비밀번호가 일치하지 않습니다.");
         } else {
-            setPasswordConfirmMessage("비밀번호가 일치합니다.");
             setIsPasswordConfirm(true);
+            setPasswordConfirmMessage("비밀번호가 일치합니다.");
         }
+        onChangeFormVaild();
     };
 
     const onChangeNickName = (e) => {
@@ -73,12 +86,13 @@ function SignupComponent() {
         setNickName(currentNickName);
 
         if (currentNickName.length === 0) {
-            setNickNameMessage("이름을 입력해주세요.");
             setIsNickName(false);
+            setNickNameMessage("이름을 입력해주세요.");
         } else {
-            setNickNameMessage("올바른 이름 형식입니다.");
             setIsNickName(true);
+            setNickNameMessage("올바른 이름 형식입니다.");
         }
+        onChangeFormVaild();
     };
 
     const onChangePhone = (e) => {
@@ -142,37 +156,65 @@ function SignupComponent() {
 
     return (
         <>
-            <div className="grid grid-cols-1 justify-items-center">
-                <div className="text-[#1c77c3] text-[20px] font-bold font-['Noto Sans']">
+            
+            <div className="grid grid-cols-1 justify-items-center mt-20 mb-20">
+                <div className="me-36 ms-5 text-[#1c77c3] text-[20px] font-bold font-['Noto Sans']">
                     회원가입을 위해<br />정보를 입력해주세요
                 </div>
-                <div className="w-[350px] [h-0px] mt-2 mb-2 border border-gray-200 "></div>
-                <div className="text-center">
-                    <form className="w-[350px] border border-[#1c77c3] rounded-lg shadow-lg shadow-blue-500/50">
-                        <input id="email" name="email" value={email} onChange={onChangeEmail} className="w-5/6 h-10 border border-gray-400 hover:border-[#1c77c3] mt-6 mb-3 rounded-lg" placeholder=" 이메일 입력"></input>
-                        <p className="message">{emailMessage}</p>
+                <div className="w-[340px] [h-0px] ms-8 mt-2 mb-2 border border-gray-200 "></div>
+                <div className="w-full max-w-xs">
+                    <form className="px-8 pt-6 pb-8 w-[350px] border border-[#1c77c3] rounded-lg shadow-lg shadow-blue-500/50">
+                        <div className="mb-5">
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                아이디 *
+                            </label>
+                            <input id="email" name="email" value={email} onChange={onChangeEmail} className="focus:outline-none ps-3 drop-shadow-md w-full h-10 border border-gray-400 hover:border-[#1c77c3] mb-3 rounded-lg" placeholder=" 이메일 입력"></input>
+                            <p className={`message ${isEmail ? 'text-green-600' : 'text-red-600'} text-xs`}>{emailMessage}</p>
+                        </div>
+                            
+                        <div className="mb-5">
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                비밀번호 *
+                            </label>
+                            <input type="password" id="password" name="password" value={password} onChange={onChangePassword} className="focus:outline-none ps-3 drop-shadow-md w-full h-10 border border-gray-400 hover:border-[#1c77c3] mb-3 rounded-lg" placeholder=" 비밀번호 입력" />
+                            <p className={`message ${isPassword ? 'text-green-600' : 'text-red-600'} text-xs`}>{passwordMessage}</p>
+                        </div>
 
-                        <input id="password" name="password" value={password} onChange={onChangePassword} className="w-5/6 h-10 border border-gray-400 hover:border-[#1c77c3] mt-3 mb-3 rounded-lg" placeholder=" 비밀번호 입력" />
-                        <p className="message">{passwordMessage}</p>
+                        <div className="mb-5">
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                비밀번호 재입력 *
+                            </label>
+                            <input type="password" id="passwordConfirm" name="passwordConfirm" value={passwordConfirm} onChange={onChangePasswordConfirm} className="focus:outline-none ps-3 drop-shadow-md w-full h-10 border border-gray-400 hover:border-[#1c77c3] mb-3 rounded-lg" placeholder=" 비밀번호 확인" />
+                            <p className={`message ${isPasswordConfirm ? 'text-green-600' : 'text-red-600'} text-xs`}>{passwordConfirmMessage}</p>
+                        </div>
 
-                        <input id="passwordConfirm" name="passwordConfirm" value={passwordConfirm} onChange={onChangePasswordConfirm} className="w-5/6 h-10 border border-gray-400 hover:border-[#1c77c3] mt-3 mb-3 rounded-lg" placeholder=" 비밀번호 확인" />
-                        <p className="message">{passwordConfirmMessage}</p>
+                        <div className="mb-5">
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                휴대폰 번호 *
+                            </label>
+                            <input id="phone" name="phone" value={phone} onChange={onChangePhone} className="focus:outline-none ps-3 drop-shadow-md w-full h-10 border border-gray-400 hover:border-[#1c77c3] mb-3 rounded-lg" placeholder=" 휴대폰 번호 입력" />
+                            <p className={`message ${isPhone ? 'text-green-600' : 'text-red-600'} text-xs`}>{phoneMessage}</p>
+                        </div>
 
-                        <input id="phone" name="phone" value={phone} onChange={onChangePhone} className="w-5/6 h-10 border border-gray-400 hover:border-[#1c77c3] mt-3 mb-3 rounded-lg" placeholder=" 휴대폰 번호 입력" />
-                        <p className="message">{phoneMessage}</p>
-
-                        <input id="nickName" name="nickName" value={nickName} onChange={onChangeNickName} className="w-5/6 h-10 border border-gray-400 hover:border-[#1c77c3] mt-3 mb-3 rounded-lg" placeholder=" 닉네임 입력" />
-                        <p className="message">{nickNameMessage}</p>
-
+                        <div className="mb-5">
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                이름 *
+                            </label>
+                            <input id="nickName" name="nickName" value={nickName} onChange={onChangeNickName} className="focus:outline-none ps-3 drop-shadow-md w-full h-10 border border-gray-400 hover:border-[#1c77c3] mb-3 rounded-lg" placeholder=" 닉네임 입력" />
+                            <p className={`message ${isNickName ? 'text-green-600' : 'text-red-600'} text-xs`}>{nickNameMessage}</p>
+                        </div>
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                선호 워케이션 지역
+                        </label>
                         <div className="flex">
-                            <select id="sido" name="sido" value={sido} onChange={onChangeSido} class="ms-7 me-0.5 mt-3 mb-3 bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 p-2.5 dark:bg-gray-700 dark:border-blue-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <select id="sido" name="sido" value={sido} onChange={onChangeSido} class="me-0.5 mb-3 bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 p-2.5 dark:bg-gray-700 dark:border-blue-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option selected>시/도 입력</option>
                                 <option value="US">United States</option>
                                 <option value="CA">Canada</option>
                                 <option value="FR">France</option>
                                 <option value="DE">Germany</option>
                             </select>
-                            <select id="gugun" name="gugun" value={gugun} onChange={onChangeGugun} class="ms-8 mt-3 mb-3 bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <select id="gugun" name="gugun" value={gugun} onChange={onChangeGugun} class="ms-8 mb-3 bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option selected>시/구/군 입력</option>
                                 <option value="US">United States</option>
                                 <option value="CA">Canada</option>
@@ -181,7 +223,7 @@ function SignupComponent() {
                             </select>
                         </div>
 
-                        <button type="button" onClick={userRegister} className="w-5/6 h-10 bg-[#1c77c3] text-white border rounded-[10px] mt-3 mb-6">회원가입</button>
+                        <button type="button" onClick={userRegister}  disabled={!isFormVaild} className={`w-full h-10 border rounded-[10px] mt-3 mb-6 ${isFormVaild ? 'drop-shadow-md bg-[#1c77c3] text-white' : 'bg-gray-300 text-gray-500'}`}>회원가입</button>
                     </form>
                 </div>
             </div>
