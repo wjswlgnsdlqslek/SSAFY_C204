568f97bee356290e907bf0e0af9ec5e75be4b490
전지훈 <ggh2111@naver.com>
2024-07-31 10:51:11 +0900

    fix(대시보드): 주석 제거

--- frontend/src/components/common/customModal.jsx
+++ frontend/src/components/common/customModal.jsx
@@ -1,33 +1,3 @@
-// // src/Modal.js
-// import React from "react";
-// import ReactDOM from "react-dom";
-
-// const CustomModal = ({ isOpen, children, onClose }) => {
-//   if (!isOpen) return null;
-
-//   return ReactDOM.createPortal(
-//     <div
-//       className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-10"
-//       onClick={onClose}
-//     >
-//       <div
-//         className="bg-white p-6 rounded-lg relative"
-//         onClick={(e) => e.stopPropagation()}
-//       >
-//         <button
-//           className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
-//           onClick={onClose}
-//         >
-//           &times;
-//         </button>
-//         {children}
-//       </div>
-//     </div>,
-//     document.getElementById("portal-root")
-//   );
-// };
-
-// export default CustomModal;
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
