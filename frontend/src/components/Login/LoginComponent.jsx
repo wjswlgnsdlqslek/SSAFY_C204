bea2ff534b40728289397d860509bedd76b8f532
전지훈 <ggh2111@naver.com>
2024-07-31 15:06:29 +0900

    style(로그인/회원가입): 로그인, 회원가입 모바일 버전 모바일 상태일 때 레이아웃 변경 및 코드 정리

--- frontend/src/components/Login/LoginComponent.jsx
+++ frontend/src/components/Login/LoginComponent.jsx
@@ -1,113 +1,137 @@
-import React from "react";
-import { useState } from "react";
-
-import { useNavigate, Link } from "react-router-dom";
-import Swal from "sweetalert2";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import useUserStore from "../../store/userStore";
-
+import useDeviceStore from "../../store/deviceStore";
 
 function LoginComponent() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const isMobile = useDeviceStore((state) => state.isMobile);
 
-    const navigate = useNavigate();
+  const onChangeEmail = (e) => setEmail(e.target.value);
+  const onChangePassword = (e) => setPassword(e.target.value);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const goToSignup = () => navigate("/signup");
 
-    const [isEmail, setIsEmail] = useState("");
-    const [isPassword, setIsPassword] = useState("");
-
-    const onChangeEmail = (e) => {
-        const currentEmail = e.target.value;
-        setEmail(currentEmail);
-        // const emailRegEx = /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
-        // if (!emailRegEx.test(currentEmail)) {
-        //     setIsEmail(false);
-        //     setEmailMessage("올바른 이메일 형식이 아닙니다.");
-        // } else {
-        //     setIsEmail(true);
-        //     setEmailMessage("올바른 이메일 형식입니다.");
-        // }
-        // onChangeFormVaild();
-    };
-    
-    const onChangePassword = (e) => {
-        const currentPassword = e.target.value;
-        setPassword(currentPassword);
-        // const passwordRegExp = /^[A-Za-z0-9]{8,20}$/
-
-        // if (!passwordRegExp.test(currentPassword)) {
-        //     setIsPassword(false);
-        //     setPasswordMessage("8~20자 사이의 숫자 + 영문자로 입력해주세요.")
-        // } else {
-        //     setIsPassword(true);
-        //     setPasswordMessage("안전한 비밀번호 입니다.")
-        // }
-        // onChangeFormVaild();
+  const userLogin = async () => {
+    const user = { email, password };
+    await useUserStore.getState().loginFunc(user);
+    if (useUserStore.getState().isLogin) {
+      navigate("/");
     }
+  };
 
-    const goToSignup = () => {
-        navigate('/signup');
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      userLogin();
     }
+  };
 
-    const createUserObject = () => {
-        let user = {
-            email,
-            password
-        }
-
-        return user
-    }
-
-    const userLogin = async () => {
-        // const user = createUserObject();
-        await useUserStore.getState().loginFunc(createUserObject())
-        console.log(useUserStore.getState().isLogin)
-        if (useUserStore.getState().isLogin) {
-            navigate('/');
-        }
-    }
-
-    const handleKeyDown = (e) => {
-        if (e.key === 'Enter') {
-            userLogin();
-        }
-    }
-
-    return (
-        <>
-            <div className="grid grid-cols-1 justify-items-center mt-20 mb-20">
-                <div className="me-36 ms-11 text-[#1c77c3] text-[20px] font-bold font-['Noto Sans']">
-                    로그인 하고<br />WAVA를 시작해보세요.
-                </div>
-                <div className="w-[340px] [h-0px] ms-8 mt-2 mb-2 border border-gray-200 "></div>
-                <div className="w-full max-w-xs">
-                    <form onKeyDown={handleKeyDown} className="px-8 pt-6 pb-8 w-[350px] border border-[#1c77c3] rounded-lg shadow-lg shadow-blue-500/50">
-                        <div className="mb-3">
-                            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                                아이디
-                            </label>
-                            <input id="email" name="email" value={email} onChange={onChangeEmail} className="focus:outline-none ps-3 drop-shadow-md w-full h-10 border border-gray-400 hover:border-[#1c77c3] mb-3 rounded-lg" placeholder=" 이메일 입력"></input>
-                            {/* <p className={`message ${isEmail ? 'text-green-600' : 'text-red-600'} text-xs`}>{emailMessage}</p> */}
-                        </div>
-                            
-                        <div>
-                            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                                비밀번호
-                            </label>
-                            <input type="password" id="password" name="password" value={password} onChange={onChangePassword} className="focus:outline-none ps-3 drop-shadow-md w-full h-10 border border-gray-400 hover:border-[#1c77c3] mb-3 rounded-lg" placeholder=" 비밀번호 입력" />
-                            {/* <p className={`message ${isPassword ? 'text-green-600' : 'text-red-600'} text-xs`}>{passwordMessage}</p> */}
-                        </div>
-
-                        <button type="button" onClick={userLogin} className={`w-full h-10 border rounded-[10px] mt-3 mb-1 drop-shadow-md bg-[#1c77c3] text-white`}>로그인</button>
-                        <div className="text-center">
-                            <span className="text-xs text-gray-400 hover:text-blue-500 cursor-pointer" onClick={goToSignup}>회원가입</span>
-                        </div>
-                    </form>
-
-                </div>
+  return (
+    <div
+      className={`
+      ${
+        isMobile
+          ? "flex flex-col items-center justify-center min-h-screen max-h-screen w-screen overflow-auto p-4"
+          : "grid grid-cols-1 justify-items-center mt-20 mb-20"
+      }
+    `}
+    >
+      <div
+        className={`
+        ${isMobile ? "w-full max-w-[90%] scale-90" : ""}
+      `}
+      >
+        <div
+          className={`
+          ${
+            isMobile
+              ? "text-[#1c77c3] text-xl font-bold mb-4"
+              : "text-[#1c77c3] text-[20px] font-bold font-['Noto Sans']"
+          }
+        `}
+        >
+          로그인 하고
+          <br />
+          WAVA를 시작해보세요.
+        </div>
+        <div
+          className={`
+          ${
+            isMobile
+              ? "w-full h-px bg-gray-200 my-4"
+              : "w-[350px] [h-0px] mt-2 mb-2 border border-gray-200"
+          }
+        `}
+        ></div>
+        <div className="w-full max-w-xs">
+          <form
+            onKeyDown={handleKeyDown}
+            className={`
+            ${
+              isMobile
+                ? "space-y-4"
+                : "px-8 pt-6 pb-8 w-[350px] border border-[#1c77c3] rounded-lg shadow-lg shadow-blue-500/50"
+            }
+          `}
+          >
+            <div className="mb-3">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                아이디
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                value={email}
+                onChange={onChangeEmail}
+                className="focus:outline-none ps-3 drop-shadow-md w-full h-10 border border-gray-400 hover:border-[#1c77c3] mb-3 rounded-lg"
+                placeholder="이메일 입력"
+              />
             </div>
-        </>
-    );
+
+            <div>
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                비밀번호
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                value={password}
+                onChange={onChangePassword}
+                className="focus:outline-none ps-3 drop-shadow-md w-full h-10 border border-gray-400 hover:border-[#1c77c3] mb-3 rounded-lg"
+                placeholder="비밀번호 입력"
+              />
+            </div>
+
+            <button
+              type="button"
+              onClick={userLogin}
+              className="w-full h-10 border rounded-[10px] mt-3 mb-1 drop-shadow-md bg-[#1c77c3] text-white"
+            >
+              로그인
+            </button>
+            <div className="text-center">
+              <span
+                className="text-xs text-gray-400 hover:text-blue-500 cursor-pointer"
+                onClick={goToSignup}
+              >
+                회원가입
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginComponent;
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,11 +1,8 @@
 import { Fragment } from "react";
-import LoginComponent from "../components/Login/LoginComponent"
+import LoginComponent from "../components/Login/LoginComponent";
 
 function LoginPage() {
-  // return <div className="px-24">로그인 페이지</div>;
-  return (
-    <LoginComponent />
-  );
+  return <LoginComponent />;
 }
 
 export default LoginPage;
