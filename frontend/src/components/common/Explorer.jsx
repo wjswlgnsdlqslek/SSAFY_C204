2d356ae06b3c9a55b4ca986a2bea4bd2f745b930
한세훈 <atdark04@gmail.com>
2024-07-31 13:27:53 +0900

    style(채널): explorer 수정

--- frontend/src/components/common/Explorer.jsx
+++ frontend/src/components/common/Explorer.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
 import useDeviceStore from "../../store/deviceStore";
 import { NavLink } from "react-router-dom";
-import { BeakerIcon } from "@heroicons/react/24/solid";
+import {
+  HomeIcon,
+  CalendarDaysIcon,
+  RectangleGroupIcon,
+} from "@heroicons/react/24/outline";
 
 const Explorer = () => {
   const isMobile = useDeviceStore((state) => state.isMobile);
@@ -24,61 +28,32 @@ const Explorer = () => {
           isMobile ? "w-[42px]" : "w-12 min-w-[48px]"
         } max-w-[110px]`}
       >
-        <div
-          className={`bg-base-300 text-mainTxt h-full min-h-[500px]  ${
-            animate ? "animate-slideIn" : ""
-          }}`}
-        >
-          <div className="pt-5"></div>
-          <NavLink to="/">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-9 ms-2 mb-2 pt-1 hover:text-btnBlue"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+        <div className={`bg-base-300 text-mainTxt h-full min-h-[500px] `}>
+          <NavLink className="hover:text-btnBlue" to="/">
+            <div className="py-2.5 flex justify-center items-center">
+              <HomeIcon
+                className={`size-8 ${animate ? "animate-dropIn" : ""}`}
               />
-            </svg>
+            </div>
           </NavLink>
-          <div className="pt-5"></div>
-          <NavLink to="/channel">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-8 ms-2 mt-2 mb-1 hover:text-btnBlue"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z"
+          <NavLink className="hover:text-btnBlue" to="/channel">
+            <div className="py-2.5 flex justify-center items-center">
+              <RectangleGroupIcon
+                stroke="currentColor"
+                className={`size-8  hover:text-btnBlue  ${
+                  animate ? "animate-dropIn" : ""
+                }`}
               />
-            </svg>
+            </div>
           </NavLink>
-          <div className="pt-5"></div>
-          <NavLink to="/dashboard">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-8 ms-2 mt-2 hover:text-btnBlue"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
+          <NavLink className="hover:text-btnBlue" to="/dashboard">
+            <div className="py-2.5 flex justify-center items-center">
+              <CalendarDaysIcon
+                className={`size-8  hover:text-btnBlue ${
+                  animate ? "animate-dropIn" : ""
+                }`}
               />
-            </svg>
+            </div>
           </NavLink>
         </div>
       </div>
