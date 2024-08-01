4f5819d29136e50902d120144c80cb1fda842af7
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2024-08-01 17:42:16 +0900

    feat(dateTime): 생성시간 자동 저장

--- backend/src/main/java/com/worq/worcation/WorcationApplication.java
+++ backend/src/main/java/com/worq/worcation/WorcationApplication.java
@@ -2,8 +2,10 @@ package com.worq.worcation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WorcationApplication {
 
 	public static void main(String[] args) {
