9fd28fa2bb077a1ee3d078a10b2d16ae7451c428
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2024-08-01 17:40:29 +0900

    feat(redis): 생성되어있는 키에 대한 데이터 추가

--- backend/src/main/java/com/worq/worcation/common/util/RedisUtil.java
+++ backend/src/main/java/com/worq/worcation/common/util/RedisUtil.java
@@ -11,17 +11,42 @@ import java.util.concurrent.TimeUnit;
 public class RedisUtil {
     private final RedisTemplate<String,Object> redisTemplate;
 
+    /**
+     * 키, 값, TTL 시간 설정하여 삽입
+     * @param key
+     * @param value
+     * @param expiredTime
+     */
     public void setData(String key, String value, Long expiredTime) {
         redisTemplate.opsForValue()
                 .set(key, value, expiredTime, TimeUnit.MILLISECONDS);
     }
 
+    /**
+     * 키에대한 값 가져오기
+     * @param key
+     * @return
+     */
     public String getData(String key) {
         return (String) redisTemplate.opsForValue()
                 .get(key);
     }
 
+    /**
+     * 키 삭제
+     * @param key
+     */
     public void deleteData(String key) {
         redisTemplate.delete(key);
+    }
+
+    /**
+     * 현재 생성되어있는 키에 값 추가하기
+     * @param key
+     * @param value
+     */
+    public void appendData(String key, String value) {
+        redisTemplate.opsForValue()
+                .append(key, value);
     }
 }
