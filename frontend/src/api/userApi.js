f3605dc607ae297808d37272602a5b443f1c8b9c
한세훈 <atdark04@gmail.com>
2024-08-01 10:38:48 +0900

    refactor(axios): 공통 통신 인스턴스 변경

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,27 +1,22 @@
-import { localAxios } from "../util/http-commons";
+import { localAxios as local } from "../util/http-commons";
 
-const local = localAxios();
+// const local = localAxios();
 
 // 사용자 회원가입
 async function register(newUser, success, fail) {
-    await local.post(`user/signup`, newUser).then(success).catch(fail);
+  await local.post(`user/signup`, newUser).then(success).catch(fail);
 }
 
 async function login(user, success, fail) {
-    await local.post(`user/login`, user).then(success).catch(fail);
+  await local.post(`user/login`, user).then(success).catch(fail);
 }
 
 async function logout(userInfo, success, fail) {
-    await local.post(`user/logout`, null, userInfo).then(success).catch(fail);
+  await local.post(`user/logout`, null, userInfo).then(success).catch(fail);
 }
 
 async function tokenRegeneration(user, success, fail) {
-    await local.post(`user/reissue`, user).then(success).catch(fail);
+  await local.post(`user/reissue`, user).then(success).catch(fail);
 }
 
-export {
-    register,
-    login,
-    logout,
-    tokenRegeneration
-};
+export { register, login, logout, tokenRegeneration };
--- frontend/src/util/http-commons.js
+++ frontend/src/util/http-commons.js
@@ -1,12 +1,36 @@
 import axios from "axios";
 
-function localAxios() {
-    const instance = axios.create({
-        baseURL: process.env.REACT_APP_SERVER_ADDRESS
-    });
-    instance.defaults.headers.common["Authorization"] = "";
-    instance.defaults.headers.common["Content-Type"] = "application/json";
-    return instance;
-}
+// function localAxios() {
+//   const instance = axios.create({
+//     baseURL: process.env.REACT_APP_SERVER_ADDRESS,
+//   });
+//   instance.defaults.headers.common["Authorization"] = "";
+//   instance.defaults.headers.common["Content-Type"] = "application/json";
+
+//   return instance;
+// }
+
+// export { localAxios };
+
+// 싱글턴 패턴으로 axios 인스턴스 생성
+const localAxios = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_ADDRESS,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// 요청 인터셉터 설정
+localAxios.interceptors.request.use(
+  (config) => {
+    // 로컬 스토리지에서 토큰을 읽어오기
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export { localAxios };
