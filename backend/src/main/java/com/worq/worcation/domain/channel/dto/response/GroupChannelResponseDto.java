bf8df05d1bcdcf45e832c153659ef3e7ef3df0d5
이병수 <evil55@naver.com>
2024-08-02 18:03:05 +0900

    feat (개발) : 채널 crud 개발중.

---
+++ backend/src/main/java/com/worq/worcation/domain/channel/dto/response/GroupChannelResponseDto.java
@@ -0,0 +1,20 @@
+package com.worq.worcation.domain.channel.dto.response;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GroupChannelResponseDto {
+
+
+    public long id;
+    public long userId;
+    public String sido;
+    public String gugun;
+    public String roomTitle;
+    public String description;
+
+
+}
---
+++ backend/src/main/java/com/worq/worcation/domain/channel/service/GroupChannelServiceImpl.java
@@ -0,0 +1,13 @@
+package com.worq.worcation.domain.channel.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GroupChannelServiceImpl {
+
+
+
+}
