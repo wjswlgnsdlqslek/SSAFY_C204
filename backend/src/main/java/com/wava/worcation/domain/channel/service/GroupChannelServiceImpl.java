158fff186f88514835feb3d696d992600dd4bac8
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2024-08-06 15:47:24 +0900

    refactor(group): Setter 제거 후 Builder 패턴 적용

--- backend/src/main/java/com/wava/worcation/domain/channel/service/GroupChannelServiceImpl.java
+++ backend/src/main/java/com/wava/worcation/domain/channel/service/GroupChannelServiceImpl.java
@@ -4,9 +4,12 @@ package com.wava.worcation.domain.channel.service;
 import com.wava.worcation.common.jwt.TokenProvider;
 import com.wava.worcation.common.response.ApiResponse;
 import com.wava.worcation.domain.channel.domain.Channel;
+import com.wava.worcation.domain.channel.domain.ChannelUser;
 import com.wava.worcation.domain.channel.dto.request.GroupChannelRequestDto;
 import com.wava.worcation.domain.channel.dto.response.GroupChannelResponseDto;
+import com.wava.worcation.domain.channel.enums.ChannelType;
 import com.wava.worcation.domain.channel.repository.ChannelRepository;
+import com.wava.worcation.domain.channel.repository.ChannelUserRepository;
 import com.wava.worcation.domain.user.domain.User;
 import com.wava.worcation.domain.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
@@ -28,9 +31,7 @@ public class GroupChannelServiceImpl implements GroupChannelService {
     private final ChannelRepository channelRepository;
     private final UserRepository userRepository;
     private final TokenProvider tokenProvider;
-
-
-
+    private final ChannelUserRepository channelUserRepository;
 
     /**
      *
@@ -38,37 +39,39 @@ public class GroupChannelServiceImpl implements GroupChannelService {
      * @ 작성일   : 2024-08-03
      * @ 설명     :
      * @param groupChannelRequestDto 모임채널 생성 데이터
-     * @param token 인증 토큰
+     * @param user 유저 객체
      * @return  201
      * @return 만들어진 모임채널 정보
      */
     @Override
     @Transactional
-    public ResponseEntity<ApiResponse<GroupChannelResponseDto>> createGroupChannel(GroupChannelRequestDto groupChannelRequestDto, String token) {
-
-        Authentication authentication = tokenProvider.getAuthentication(token);
-        User userOpt  = userRepository.findByEmail(authentication.getName()).orElseThrow();
-
+    public ResponseEntity<ApiResponse<GroupChannelResponseDto>> createGroupChannel(GroupChannelRequestDto groupChannelRequestDto, User user) {
         Channel channel = Channel.builder()
-                .user(userOpt)
+                .user(user)
                 .channelDescription(groupChannelRequestDto.getDescription())
                 .channelTitle(groupChannelRequestDto.getRoomTitle())
-                .channelType("C001")    //C001 : 그룹 ,  C002 : 피드
+                .channelType(ChannelType.GROUP.getCode())    //C001 : 그룹 ,  C002 : 피드
                 .channelSido(groupChannelRequestDto.getSido())
                 .channelSigungu(groupChannelRequestDto.getGugun())
                 .build();
 
          channelRepository.save(channel);
 
-         GroupChannelResponseDto groupChannelResponseDto = new GroupChannelResponseDto();
-         groupChannelResponseDto.setId(channel.getId());
-         groupChannelResponseDto.setUserId(userOpt.getId());
-         groupChannelResponseDto.setDescription(channel.getChannelDescription());
-         groupChannelResponseDto.setRoomTitle(channel.getChannelTitle());
-         groupChannelResponseDto.setSido(channel.getChannelSido());
-         groupChannelResponseDto.setRoomTitle(channel.getChannelTitle());
-         groupChannelResponseDto.setDescription(channel.getChannelDescription());
+         channelUserRepository.save(
+                 ChannelUser.builder()
+                         .user(user)
+                         .channel(channel)
+                         .build());
 
+         GroupChannelResponseDto groupChannelResponseDto = GroupChannelResponseDto.builder()
+                 .id(channel.getId())
+                 .userId(channel.getUser().getId())
+                 .description(channel.getChannelDescription())
+                 .roomTitle(channel.getChannelTitle())
+                 .sido(channel.getChannelSido())
+                 .gugun(channel.getChannelSigungu())
+                 .userCount(channelUserRepository.countByChannelId(channel.getId()))
+                 .build();
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponse.success(groupChannelResponseDto));
@@ -83,35 +86,20 @@ public class GroupChannelServiceImpl implements GroupChannelService {
      * @return 모든 모임채널 정보
      */
     @Override
-    public ResponseEntity<ApiResponse<List<GroupChannelResponseDto>>> showAllGroupChannel(String token) {
-
-        Authentication authentication = tokenProvider.getAuthentication(token);
-        User userOpt  = userRepository.findByEmail(authentication.getName()).orElseThrow() ;
-
-        List<Channel> channelList = channelRepository.findAll();
+    public ResponseEntity<ApiResponse<List<GroupChannelResponseDto>>> showAllGroupChannel() {
+        List<Channel> channelList = channelRepository.findByChannelType(ChannelType.GROUP.getCode());
         List<GroupChannelResponseDto> groupChannelResponseDtoList = new ArrayList<>();
-        GroupChannelResponseDto groupChannelResponseDto = null;
         for (Channel channel : channelList) {
-            groupChannelResponseDto = new GroupChannelResponseDto();
-            groupChannelResponseDto.setId(channel.getId());
-            groupChannelResponseDto.setUserId(channel.getUser().getId());
-            groupChannelResponseDto.setDescription(channel.getChannelDescription());
-            groupChannelResponseDto.setRoomTitle(channel.getChannelTitle());
-            groupChannelResponseDto.setSido(channel.getChannelSido());
-            groupChannelResponseDto.setGugun(channel.getChannelSigungu());
-            groupChannelResponseDtoList.add(groupChannelResponseDto);
+            groupChannelResponseDtoList.add(GroupChannelResponseDto.builder()
+                    .id(channel.getId())
+                    .userId(channel.getUser().getId())
+                    .description(channel.getChannelDescription())
+                    .roomTitle(channel.getChannelTitle())
+                    .sido(channel.getChannelSido())
+                    .gugun(channel.getChannelSigungu())
+                    .userCount(channelUserRepository.countByChannelId(channel.getId()))
+                    .build());
         }
-
-
-
-        //채널이 없을 경우 'mess
-//        if(groupChannelResponseDtoList.isEmpty()) {
-//            return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(null,"모임채널이 없습니다."));
-//
-//        }
-//        else {
-//            return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(groupChannelResponseDtoList));
-//        }
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(groupChannelResponseDtoList));
     }
 
