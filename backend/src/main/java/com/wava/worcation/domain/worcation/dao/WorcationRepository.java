fc3ecc47688c6ebe9ad7bf49b9ac8c7ad9a5afdc
최승호 <148317140+S-Choi-1997@users.noreply.github.com>
2024-08-06 17:56:07 +0900

    fix: worcation conflice issue

--- backend/src/main/java/com/wava/worcation/domain/worcation/dao/WorcationRepository.java
+++ backend/src/main/java/com/wava/worcation/domain/worcation/dao/WorcationRepository.java
@@ -1,12 +1,12 @@
 // src/main/java/com/worq/worcation/domain/worcation/dao/WorcationRepository.java
 package com.wava.worcation.domain.worcation.dao;
 
-import com.wava.worcation.domain.user.domain.User;
+import com.wava.worcation.domain.worcation.domain.Worcation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.wava.worcation.domain.worcation.domain.Worcation;
 
 @Repository
 public interface WorcationRepository extends JpaRepository<Worcation, Long> {
     Worcation findByUserId(Long userId);
+    boolean existsByUserId(Long userId);
 }
