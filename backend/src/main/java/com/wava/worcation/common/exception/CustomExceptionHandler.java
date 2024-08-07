a14a589aa4c8050a8966524823240bb149307865
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2024-08-08 01:54:01 +0900

    rename(exceptionHandler): 어노테이션이랑 이름이 겹처 이름 변경

--- backend/src/main/java/com/wava/worcation/common/exception/CustomExceptionHandler.java
+++ backend/src/main/java/com/wava/worcation/common/exception/CustomExceptionHandler.java
@@ -2,10 +2,16 @@ package com.wava.worcation.common.exception;
 
 import com.wava.worcation.common.response.ApiResponse;
 import com.wava.worcation.common.response.ErrorCode;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
@@ -17,7 +23,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  */
 @Slf4j
 @RestControllerAdvice
-public class ExceptionHandler {
+public class CustomExceptionHandler {
 
 
     /**
@@ -31,7 +37,7 @@ public class ExceptionHandler {
      * 발생한 CustomException 객체를 매개변수로 받아서 처리합니다.
      * ErrorCode에서 상태 코드와 메시지를 추출하여 응답을 생성합니다.
      */
-    @org.springframework.web.bind.annotation.ExceptionHandler(CustomException.class)
+    @ExceptionHandler(CustomException.class)
     public ResponseEntity<ApiResponse<String>> handleCustomException(CustomException customException) {
         ErrorCode errorCode = customException.getErrorCode();
         return ResponseEntity.status(errorCode.getStatus())
@@ -42,14 +48,39 @@ public class ExceptionHandler {
      * 작성자   : 이병수
      * 작성일   : 2024-08-04
      * 설명     : 모든 Exception을 처리하는 메서드
-     * @param ex 발생한 Exception 객체
+//     * @param ex 발생한 Exception 객체
      * @return HTTP 상태 코드와 ApiResponse를 포함한 응답 객체 .
      *
      * 이 메서드는 CustomException을 제외한 모든 예외가 발생했을 때 호출됩니다.
      */
-    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<String>> handleException(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR, "일시적인 오류가 발생하였습니다. 다시 시도해주세요."));
+    }
+
+    @ExceptionHandler(MalformedJwtException.class)
+    public ResponseEntity<ApiResponse> handleMalformedJwtException() {
+        return ResponseEntity.status(ErrorCode.WRONG_TYPE_TOKEN.getStatus()).body(ApiResponse.error(ErrorCode.WRONG_TYPE_TOKEN));
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ApiResponse> handleExpiredJwtException() {
+        return ResponseEntity.status(ErrorCode.EXPIRED_TOKEN.getStatus()).body(ApiResponse.error(ErrorCode.EXPIRED_TOKEN));
+    }
+
+    @ExceptionHandler(UnsupportedJwtException.class)
+    public ResponseEntity<ApiResponse> handleAuthenticationException() {
+        return ResponseEntity.status(ErrorCode.UNSUPPORTED_TOKEN.getStatus()).body(ApiResponse.error(ErrorCode.UNSUPPORTED_TOKEN));
+    }
+
+    @ExceptionHandler(IllegalAccessError.class)
+    public ResponseEntity<ApiResponse> handleIllegalAccessException() {
+        return ResponseEntity.status(ErrorCode.UNKNOWN_TOKEN.getStatus()).body(ApiResponse.error(ErrorCode.UNKNOWN_TOKEN));
+    }
+
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ApiResponse> handlerJwtException() {
+        return ResponseEntity.status(401).body(ApiResponse.error(ErrorCode.EXPIRED_TOKEN));
     }
 }
