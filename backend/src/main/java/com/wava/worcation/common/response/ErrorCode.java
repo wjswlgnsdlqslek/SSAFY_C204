e57a25ad28e4b187452c7e20016931ec3858f55c
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2024-08-07 16:16:45 +0900

    feat(error): 지도 핀 생성 에러 코드 추가

--- backend/src/main/java/com/wava/worcation/common/response/ErrorCode.java
+++ backend/src/main/java/com/wava/worcation/common/response/ErrorCode.java
@@ -21,7 +21,8 @@ public enum ErrorCode {
     WRONG_TYPE_TOKEN(HttpStatus.valueOf(401), "잘못된 타입의 토큰입니다."),
     EXPIRED_TOKEN(HttpStatus.valueOf(402), "만료된 토큰입니다."),
     UNSUPPORTED_TOKEN(HttpStatus.valueOf(403), "지원되지 않는 토큰입니다."),
-    PLAN_NOT_FOUND(HttpStatus.NOT_FOUND,"일정을 찾을 수 없습니다.");
+    PLAN_NOT_FOUND(HttpStatus.NOT_FOUND,"일정을 찾을 수 없습니다."),
+    INVALID_PIN_ORDER(HttpStatus.CONFLICT,"핀 번호가 중복되었습니다.");
 
     private HttpStatus status;
     private String message;
