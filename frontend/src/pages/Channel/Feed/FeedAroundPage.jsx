a0fc5fc830a328c4b381b526fdccf18dba84d1e8
한세훈 <atdark04@gmail.com>
2024-08-07 16:26:00 +0900

    refactor(피드):주석제거, 분기변경

--- frontend/src/pages/Channel/Feed/FeedAroundPage.jsx
+++ frontend/src/pages/Channel/Feed/FeedAroundPage.jsx
@@ -1,62 +1,8 @@
-// import { useState } from "react";
-// import ContentDrawer from "../../../components/Channel/feed/ContentDrawer";
-// import ContentItemGrid from "../../../components/Channel/feed/ContentItemGrid";
-// import FeedSearchBar from "../../../components/Channel/feed/FeedSearchbar";
-// import { get_feedData } from "../../../api/dummy";
-
-// const contents = get_feedData;
-// // 임시 더미 데이터, api연결되면 바꿀것
-
-// function FeedAroundPage() {
-//   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-//   const [selectedContent, setSelectedContent] = useState(null);
-
-//   const handleCloseDrawer = () => {
-//     setIsDrawerOpen(false);
-//     setSelectedContent(null);
-//   };
-
-//   const handleSelectContent = (content) => {
-//     setSelectedContent(content);
-//     setIsDrawerOpen(true);
-//   };
-
-//   const searchHandle = (searchText) => {};
-
-//   return (
-//     <div className="flex h-full">
-//       <div className="flex flex-col flex-1">
-//         <FeedSearchBar searchHandle={searchHandle} />
-//         <ContentItemGrid
-//           contents={contents}
-//           onSelectContent={handleSelectContent}
-//         />
-//         <ContentDrawer
-//           isOpen={isDrawerOpen}
-//           onClose={handleCloseDrawer}
-//           content={selectedContent}
-//         />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default FeedAroundPage;
-
-// 랜덤 유저 정보 생성 후 이동
 import { useEffect, useState } from "react";
 import ContentDrawer from "../../../components/Channel/feed/ContentDrawer";
 import ContentItemGrid from "../../../components/Channel/feed/ContentItemGrid";
 import FeedSearchBar from "../../../components/Channel/feed/FeedSearchbar";
-import { get_feedData } from "../../../api/dummy";
 import { searchFeedRequest } from "../../../api/channelFeedApi";
-
-// Modify the dummy data to include author information
-// const contents = get_feedData?.data?.map((content) => ({
-//   ...content,
-//   authorId: `user-${Math.floor(Math.random() * 1000)}`, // Random user ID
-//   authorName: `User ${Math.floor(Math.random() * 1000)}`, // Random user name
-// }));
 
 function FeedAroundPage() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
