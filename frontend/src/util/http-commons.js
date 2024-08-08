6dea65b870d90e74ea92a42e0cdc8a802011c951
한세훈 <atdark04@gmail.com>
2024-08-08 17:03:01 +0900

    feat(토큰):응답 인터셉터 작성
    Back 완성되면 테스트 필요

--- frontend/src/util/http-commons.js
+++ frontend/src/util/http-commons.js
@@ -36,33 +36,51 @@ localAxios.interceptors.request.use(
   // (error) => Promise.reject(error)
 );
 
-// // 응답 인터셉터 설정
-// localAxios.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   (error) => {
-//     // 401 Unauthorized: 토큰 만료 혹은 인증 실패
-//     if (error.response.status === 401) {
-//       console.log('토큰이 만료되었습니다.');
+// 응답 인터셉터 설정
+localAxios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    // 401 Unauthorized: 토큰 만료 혹은 인증 실패
+    if (error.response.status === 401) {
+      console.log("토큰이 만료되었습니다.");
 
-//       // 토큰을 갱신하는 비동기 함수 호출 예시
-//       return refreshToken().then(newToken => {
-//         // 새 토큰으로 axios config 설정
-//         error.config.headers['Authorization'] = `Bearer ${newToken}`;
-//         // 새 토큰으로 원래 요청 재시도
-//         return axios.request(error.config);
-//       });
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+      // 토큰을 갱신하는 비동기 함수 호출,
+      return refreshToken().then((newToken) => {
+        // 새 토큰으로 axios config 설정
+        error.config.headers["Authorization"] = `Bearer ${newToken}`;
+        // 새 토큰으로 원래 요청 재시도
+        return axios.request(error.config);
+      });
+    }
+    return Promise.reject(error);
+  }
+);
 
-// function refreshToken() {
-//   // 토큰을 갱신하는 로직을 작성
-//   // 예를 들어, 서버에서 새 토큰을 발급받는 API 호출 등
-//   return axios.post('/api/refresh_token')
-//     .then(response => response.data.token);
-// }
+function refreshToken() {
+  const token = sessionStorage.getItem("accessToken");
+  const re_token = sessionStorage.getItem("refreshToken");
+  // 토큰을 갱신하는 로직을 작성
+  return axios
+    .post(
+      "/api/user/reissue",
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          refreshToken: re_token,
+        },
+      }
+    )
+    .then((response) => {
+      let accessToken = response.headers["authorization"];
+      let refreshToken = response.headers["refreshtoken"];
+      sessionStorage.setItem("accessToken", accessToken);
+      sessionStorage.setItem("refreshToken", refreshToken);
+
+      return accessToken;
+    });
+}
 
 export { localAxios };
