1c1cdc2559737d96dcc63a99a91b70dcf59651b5
한세훈 <atdark04@gmail.com>
2024-08-12 21:40:07 +0900

    fix(커서):stomp 커넥션 에러

--- frontend/src/components/Channel/group/ControllerComponent.jsx
+++ frontend/src/components/Channel/group/ControllerComponent.jsx
@@ -1,44 +1,48 @@
 import React, { useEffect, useState } from "react";
 import { VideoCameraIcon } from "@heroicons/react/24/outline";
 import { ChannelUserComponent } from "./ChannelUserComponent";
-import { groupChannelAPI } from "../../../api/groupChannelAPI"
+import { groupChannelAPI } from "../../../api/groupChannelAPI";
 
 export default function ControllerComponent(props) {
-  const { mode, setMode, groupId, onSelectUser } = props
-  const [userList, setUserList] = useState(null)
-  const [userItems, setUserItems] = useState(null)
+  const { mode, setMode, groupId, onSelectUser } = props;
+  const [userList, setUserList] = useState(null);
+  const [userItems, setUserItems] = useState(null);
 
   // const changeMode = () => {
   //   setMode(!mode);
   // };
 
   const getUserList = async () => {
-      setUserList(null);
-      try {
-        const userData = await groupChannelAPI.getChannelInfo(groupId);
-        if (userData.status === "OK") {
-          console.log(userData)
-          setUserList(userData.data.user);   
-        }
-      } catch (error) {
-        console.error("Error get channel userinfo: ", error);
+    setUserList(null);
+    try {
+      const userData = await groupChannelAPI.getChannelInfo(groupId);
+      if (userData.status === "OK") {
+        console.log(userData);
+        setUserList(userData.data.user);
       }
-  }
-  
+    } catch (error) {
+      console.error("Error get channel userinfo: ", error);
+    }
+  };
+
   useEffect(() => {
     getUserList();
-    console.log("실행됨?")
-  }, [groupId])
+    console.log("실행됨?");
+  }, [groupId]);
 
   useEffect(() => {
     if (userList) {
-      const items = userList.map((user) => 
-        <ChannelUserComponent nickName={user.nickName} profileImg={user.profile} onClick={onSelectUser} />
-      );
+      const items = userList.map((user) => (
+        <ChannelUserComponent
+          key={user.nickName}
+          nickName={user.nickName}
+          profileImg={user.profile}
+          onClick={onSelectUser}
+        />
+      ));
       setUserItems(items);
     }
-  }, [userList])
-
+  }, [userList]);
 
   return (
     <>
